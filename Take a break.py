d74529591cb62aecb4dca3eaa58c5941628ec734
armaango <armaan.goyal@gmail.com>
2016-07-11 17:18:10 -0400

    increased time counter

--- Take a break.py
+++ Take a break.py
@@ -1,5 +1,7 @@
 import time
 import webbrowser
 
-time.sleep(10)
-webbrowser.open("https://www.youtube.com/watch?v=gEc_fjqMMeE")
+print ("This program started on ", time.ctime())
+for i in range(0,3):
+    time.sleep(2*60*60)
+    webbrowser.open("https://www.youtube.com/watch?v=gEc_fjqMMeE")
