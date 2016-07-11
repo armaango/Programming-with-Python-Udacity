f45a3b09681e0b96f451f1c11e506a7ba95f0f09
armaango <armaan.goyal@gmail.com>
2016-07-11 17:49:48 -0400

    second mini project-complete

--- prank.py
+++ prank.py
@@ -1,7 +1,13 @@
 import os
 
 def rename_files():
-    file_list = os.listdir(r"/Users/AmmY/Downloads/prank")
+    file_list = os.listdir("/Users/AmmY/Downloads/prank")
     print(file_list)
+    saved_path = os.getcwd()
+    os.chdir("/Users/AmmY/Downloads/prank")
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(str.maketrans("0123456789","          ")))
 
+    os.chdir(saved_path)
+    print (os.listdir("/Users/AmmY/Downloads/prank"))
 rename_files()
